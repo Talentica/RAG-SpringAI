86ca11abd69777ec118f266a2300c96ce5e2ef13
krishnaBaile <krishna.baile@talentica.com>
2025-01-03 16:46:56 +0530

    unnecessary redis implementation removed

--- src/main/java/com/openAi/security/utils/JpaConfig.java
+++ src/main/java/com/openAi/security/utils/JpaConfig.java
@@ -1,22 +1,22 @@
-//package com.openAi.security.utils;
-//
-//import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class JpaConfig {
-//
-//    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder,
-//                                                                       DataSource dataSource) {
-//        return builder
-//                .dataSource(dataSource)
-//                .packages("com.openAi.security.entity")
-//                .persistenceUnit("default")
-//                .build();
-//    }
-//}
+package com.openAi.security.utils;
+
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder,
+                                                                       DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.openAi.security.entity")
+                .persistenceUnit("default")
+                .build();
+    }
+}
