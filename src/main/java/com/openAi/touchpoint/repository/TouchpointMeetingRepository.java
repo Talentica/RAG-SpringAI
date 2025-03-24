dba1259ac597b6c100ab05ebbdbf4c9198591c3b
Madhumitha <Madhumitha.madhuramani@talentica.com>
2025-03-24 11:03:27 +0530

    Delete Touchpoint module

--- src/main/java/com/openAi/touchpoint/repository/TouchpointMeetingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.openAi.touchpoint.repository;
-
-import com.openAi.touchpoint.entities.TouchpointMeeting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TouchpointMeetingRepository extends JpaRepository<TouchpointMeeting,Integer> {
-}
